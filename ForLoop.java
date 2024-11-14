7ddaf04d3f5384ad81d626ec7a09b2f9f7f9d4bd
Rajnish <rajnishrajak82@gmail.com>
2024-11-14 11:18:44 +0545

    New commit for loop

---
+++ ForLoop.java
@@ -0,0 +1,18 @@
+public class ForLoop {
+    public static void main(String[] args){
+        //1.Initialize
+        //2.Condition
+        //3.Update/Modify
+        for (int i=1; i<5; i++){
+            //loop statement
+            System.out.println("Loop" +1);
+        }
+
+        //Dcrement
+        int dec;
+        for(dec=5; dec>=1; dec -=2){
+            System.out.println("Loop dec by two" +dec);
+        }
+    }
+    
+}
